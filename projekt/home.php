403b9189b5b0b8762721f619844297f71f6d7fdb
Crimcia <117082169+Crimcia@users.noreply.github.com>
2025-02-03 13:04:40 +0100

    Add files via upload

---
+++ projekt/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Stack Overflow</title>
+</head>
+<body>
+    <nav>
+        <div class="logo-nav-cont">
+            <a href="index.php" class="logo-nav">
+                <img src="img/logo.png" alt="logo" width=25 height=25>
+                <h1>Stack Overflow</h1>
+            </a>
+        </div>
+        <hr>
+        <div class="login-nav-cont">
+            <a href="login.php" class='login-nav'>Log in</a>
+            <a href="sign-in.php" class='login-nav sign-in'>Sign in</a>
+        </div>
+    </nav>
+    <header>
+        <h1>Stack Overflow</h1>
+        <div>A place for all developers</div>
+    </header>
+    <main>
+        <section class='home-sect'>
+            
+        </section>
+        <section class='questions-sect'>
+
+        </section>
+        <section class='tags-sect'>
+            
+        </section>
+        <section class='users-sect'>
+            
+        </section>
+        <section class='questions-sect'>
+            
+        </section>
+    </main>
+    <script src="app.js"></script>
+</body>
+</html>
---
+++ projekt/sign-in.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Stack Overflow sign in page</title>
+</head>
+<body>
+    <nav>
+        <div class="logo-nav-cont">
+            <a href="index.php" class="logo-nav">
+                <img src="img/logo.png" alt="logo" width=25 height=25>
+                <h1>Stack Overflow</h1>
+            </a>
+        </div>
+        <hr>
+        <div class="login-nav-cont">
+            <a href="login.php" class='login-nav'>Log in</a>
+            <a href="sign-in.php" class='login-nav sign-in'>Sign in</a>
+        </div>
+    </nav>
+    <main>
+        <form action="">
+            <h1>Sign into Stack Overflow</h1>
+            <label for="name-inp">Username:</label>
+            <input type="text" id="name-inp">
+            <br>
+            <label for="password-inp">Password:</label>
+            <input type="text" id="password-inp">
+            <br>
+            <button>Log in</button>
+        </form>
+    </main>
+</body>
+</html>
